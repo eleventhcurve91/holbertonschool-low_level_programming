11d4c9804b09e4ed8ddec3cf7d578b3a09520f51
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-09 15:00:22 -0700

    \(*@*)/

---
+++ hello_world/101-quote.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Starting program
+ * Return: Always 0 (Succes)
+ * ====================================
+ * Autor: Carlos Alfonso Bustos Olaya |
+ * ====================================
+ * ***********Description**************
+ * =======================================
+ * Write a C program that prints exactly |
+ * and that piece of art is useful	 |
+ * - Dora Korpar, 2015-10-19, followed by|
+ * a new line, to the standard error.	 |
+ * =======================================
+ * -You are not allowed to use any 	 |
+ *  functions listed in the NAME section |
+ *  of the man (3) printf or man (3) puts|
+ * =======================================
+ * -Your program should return 1	 |
+ * =========================================
+ * Your program should compile without any |
+ * warnings when using the -Wall gcc option|
+ * =========================================
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
