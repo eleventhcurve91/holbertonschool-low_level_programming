49d9f266282a921c3e46af8c0e9195d7b38e4b74
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-09 14:36:45 -0700

    \(*@*)/

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,13 +3,21 @@
 /**
  * main - Starting program
  * Return: Always 0 (Succes)
- *
+ * ====================================
+ * Autor: Carlos Alfonso Bustos Olaya |
+ * ====================================
+ * ***********Description**************
+ * ======================================
+ * Write a C program that prints the    |
+ * size of various types on the computer|
+ * it is compiled and run on.		|
+ * ======================================
  */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n",sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d ", sizeof(long int), "bytes(s)\n");
-	printf("Size of a float: %d", sizeof(float), "bytes(s)\n");
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
