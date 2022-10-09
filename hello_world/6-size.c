90253f481639622bb5388b6550cbdb7b4cac6b0f
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-09 14:26:03 -0700

    \(*@*)/

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-	printf("Size of a char: ", sizeof(char) , "bytes(s)");
+	printf("Size of a char: ",sizeof(char),"bytes(s)");
+	printf("Size of an int: ",sizeof(int),"bytes(s)");
+	printf("Size of a long int: ",sizeof(long int),"bytes(s)");
+	printf("Size of a float: ",sizeof(float),"bytes(s)");
 	return (0);
 }
