9a61d769b47c4c4551abaf441deddd71b03f1317
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-08 20:57:55 -0700

    \(*@*)/

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,23 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /*
- * main - Entry point
- * 
+ * main - Starting point for program execution
+ * Description: prints "with proper grammar, but the outcome is a piece of art,"
  * Return: Always 0 (Success)
- */
-/*
- * ====================================
- * AUTOR: Carlos Alfonso Bustos Olaya |
- * ====================================
- * ************DESCRIPCION************|
- * ====================================
- * -Use the function puts	      |
- * -you are not allowed to use printf |
- * -your program should end witch the |
- * value cero			      |
- * ====================================
  */
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
