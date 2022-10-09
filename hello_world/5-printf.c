ce18ae1c7a8db7201c6d0e4a6ea8df658b6cfda6
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-08 21:15:21 -0700

    \(*@*)/

---
+++ hello_world/5-printf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * ====================================
+ * AUTOR: Carlos Alfonso Bustos Olaya |
+ * ====================================
+ * ************DESCRIPCION************|
+ * ====================================
+ * -Use the function printf 	      |
+ * -you are not allowed to use puts   |
+ * -your program should end witch the |
+ * value cero			      |
+ * ====================================
+ */
+
+/**
+ * main - Starting point for program execution
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
