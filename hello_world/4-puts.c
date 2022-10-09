0309e532b0273e431be718f3a41a93f9b8c5b703
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-08 19:15:38 -0700

    \(*@*)/

---
+++ hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/*===================================
+ *AUTOR: Carlos Alfonso Bustos Olaya |
+ *===================================
+ *           DESCRIPCION	     |
+ *===================================
+ *-Use the function puts	     |
+ *-you are not allowed to use printf |
+ *-your program should end witch the |
+ * value cero			     |
+ * ==================================
+ */
+
+ int main(void){
+
+	 printf("Programming is like building a multilingual puzzle,\n");
+	
+	 return(0);
+ 
+ }
+
