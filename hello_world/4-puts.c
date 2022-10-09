65dbcad7ddb99989e01fbbf04b0527f4d120cbda
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-08 19:20:59 -0700

    \(*@*)/

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
-/*===================================
- *AUTOR: Carlos Alfonso Bustos Olaya |
- *===================================
- *           DESCRIPCION	     |
- *===================================
- *-Use the function puts	     |
- *-you are not allowed to use printf |
- *-your program should end witch the |
- * value cero			     |
+/* ===================================
+ * AUTOR: Carlos Alfonso Bustos Olaya |
+ * ===================================
+ *            DESCRIPCION	      |
+ * ===================================
+ * -Use the function puts	      |
+ * -you are not allowed to use printf |
+ * -your program should end witch the |
+ * value cero			      |
  * ==================================
  */
 
