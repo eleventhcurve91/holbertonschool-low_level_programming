a7cbe8ba23754a752ca52f276bd5bf1a722ce7e9
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-09 14:18:58 -0700

    \(*@*)/

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,16 @@
-pass
+#include <stdio.h>
+
+/**
+ * main - Starting program
+ * Return: Always 0 (Succes)
+ *
+ */
+int main(void)
+{
+	puts("Size of a char: ",(int) sizeof(char), "bytes(s)");
+	return (0);
+	
+
+
+}
+
