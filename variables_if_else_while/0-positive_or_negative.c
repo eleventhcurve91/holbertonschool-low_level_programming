96508417c0618c5817834bd793a5719224d44ddf
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 09:02:10 -0700

    \(*#*)/

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,10 +7,10 @@ int main(void)
 	int n = rand() - RAND_MAX / 2;
 	srand(time(0));
 	if (n == 0)
-		printf("%d is zero \n", n);
+		printf("%d is zero\n", n);
 	if (n > 0)
-		printf("%d is positive \n", n);
+		printf("%d is positive\n", n);
 	if (n < 0)
-		printf("%d is negative \n", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
