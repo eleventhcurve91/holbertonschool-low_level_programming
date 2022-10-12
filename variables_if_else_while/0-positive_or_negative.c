b383c80fab4ee074837841071619d2cd46c7b4e7
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 09:07:55 -0700

    \(*#*)/

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,6 +2,22 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Starting program
+ * Return: Always 0 (Succes)
+ * ====================================
+ * Autor: Carlos Alfonso Bustos Olaya |
+ * ====================================
+ * ***********Description**************
+ * ======================================
+ * -if the number is greater than 0:	|
+ * is positive				|
+ * -if the number is 0: is zero		|
+ * -if the number is less than 0: 	|
+ * is negative				|
+ * ======================================
+ */
+
 int main(void)
 {
 	int n = rand() - RAND_MAX / 2;
