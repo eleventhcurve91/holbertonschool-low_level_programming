a45836b2e20e745758f536d1d439ea7aad5abce8
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 16:21:30 -0700

    \(*@*)/

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-	
+	srand(time(0));
+
 	int n = rand() - RAND_MAX / 2;
 
-	srand(time(0));
 	n %= 10;
 	if(n < 5)
-		printf("%d is greater than 5\n", n);
+		printf("the last digit of %d is greater than 5\n", n);
 	if(n < 0 && n != 0)
-		printf("%d is less than 6 and not 0\n", n);
+		printf("the last digit of %d is less than 6 and not 0\n", n);
 	else
-		printf("%d is 0: the string", n);
+		printf("the last digit of %d is 0: the string", n);
 	return (0);
 }
