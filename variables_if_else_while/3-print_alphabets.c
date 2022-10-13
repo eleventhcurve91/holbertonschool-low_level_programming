f98a83eb07b2da8e25dce7d2a68c026723c293ac
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 17:52:13 -0700

    \(*@*)/

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * main - Starting program
+ * Return: Always 0 (Succes)
+ * =======================================
+ * Autor: Carlos Alfonso Bustos Olaya	 |
+ * =======================================
+ * ************Description****************
+ * =======================================
+ * Write a program that prints the	 |
+ * alphabet in lowercase, and then in	 |
+ * uppercase, followed by a new line.	 |
+ * =======================================
+ */
 int main(void)
 {
 	char letter;
