5889511ae5e0d5c63071422adf55573a619d434d
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 22:43:12 -0700

    \(*@*)/

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Starting program
+ * Return: Always 0 (Succes)
+ * =========================================
+ * Autor: Carlos Alfonso Bustos Olaya	   |
+ * =========================================
+ * **************Description****************
+ * =========================================
+ * Write a program that prints the	   |
+ * lowercase alphabet in reverse, followed |
+ * by a new line.			   |
+ * =========================================
+ */
 int main(void)
 {
 	int letter = 123;
@@ -10,5 +23,6 @@ int main(void)
 		letter--;
 		putchar(letter);
 	}
+	putchar(10);
 	return (0);
 }
