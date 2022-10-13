7a8a350ffae8c7b6a741efb48483611d1849ca90
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 20:50:17 -0700

    \(*@*)/

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Starting program
+ * Return: Always 0 (Succes)
+ * =======================================
+ * Autor: Carlos Alfonso Bustos Olaya	 |
+ * =======================================
+ * ************Description****************
+ * =======================================
+ * -Write a program that prints the	 |
+ * alphabet in lowercase, followed by a	 |
+ * new line.				 |
+ * -Print all the letters except q and e |
+ * =======================================
+ */
 int main(void)
 {
 	char letter = 'a';
