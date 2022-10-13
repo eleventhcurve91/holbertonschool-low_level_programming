2c068fdf541fc71306596e021c0f516acbe6b64c
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 17:09:52 -0700

    \(*@*)/

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@
  * than 5				 |
  * -if the last digit of n is 0:	 |
  * the string and is 0			 |
- * -if the last digit of n is less than 6| 
+ * -if the last digit of n is less than 6|
  * and not 0: the string and is less	 |
  * than 6 and not			 |
  * =======================================
@@ -26,6 +26,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	rdm = n;
 	n %= 10;
 	if (n > 5)
