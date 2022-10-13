65d4a0ad3ebf4510c3ecfac50dca19bfb2279c47
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 17:35:41 -0700

    \(*@*)/

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	if (letter == 'z')
+		for (letter = 'A'; letter <= 'Z'; letter++)
+			putchar(letter);
+}
