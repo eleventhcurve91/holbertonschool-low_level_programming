43d27e6f1ce2438ab8658cb56bdb154189cd4f92
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 19:37:08 -0700

    \(*@*)/

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter == 'q' || letter == 'e')
+			putchar(letter);
+	return (0);
+}
