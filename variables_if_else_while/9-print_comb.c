35c68e0fa914d51466818323d32a02aebae72ca7
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-13 00:13:49 -0700

    \(*@*)/

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Starting program
+ * Return: Always 0 (Succes)
+ * =======================================
+ * Autor: Carlos Alfonso Bustos Olaya	 |
+ * =======================================
+ * ************Description****************
+ * =======================================
+ * -Write a program that prints all	 |
+ * possible combinations of single-digit |
+ * numbers.				 |
+ * -Numbers must be separated by ,,	 |
+ * followed by a space.			 |
+ * =======================================
+ */
 int main(void)
 {
 	int value;
