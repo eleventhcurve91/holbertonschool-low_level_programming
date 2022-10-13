4f55e6018dadae6af9ba0ff8e01d553b2470e75a
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 20:14:57 -0700

    \(*@*)/

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,7 +7,7 @@ int main(void)
 
 	for (letter == 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	while (letter == 'q'|| letter == 'e')
+	while (letter = 'q'|| letter == 'e')
 		putchar(letter+1);
 	return (0);
 }
