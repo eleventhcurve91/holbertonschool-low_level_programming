1fbb40e52ff2dfb0e91c53acecb20fa9b047ad57
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 23:31:40 -0700

    \(*@*)/

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Starting program
+ * Return: Always 0 (Succes)
+ * =======================================
+ * Autor: Carlos Alfonso Bustos Olaya	 |
+ * =======================================
+ * ************Description****************
+ * =======================================
+ * Write a program that prints all the	 |
+ * numbers of base 16 in lowercase,	 |
+ * followed by a new line.		 |
+ * =======================================
+ */
 int main(void)
 {
 	int hex = 0;
@@ -16,8 +29,8 @@ int main(void)
 			putchar(hex - 10 + 'A');
 		}
 		hex++;
-
 	}
 	putchar('\n');
+
 	return (0);
 }
