219e91b862e5dc88c965742516380db344cc8327
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 22:12:22 -0700

    \(*@*)/

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Starting program
+ * Return: Always 0 (Succes)
+ * =========================================
+ * Autor: Carlos Alfonso Bustos Olaya	   |
+ * =========================================
+ * **************Description****************
+ * =========================================
+ * -Write a program that prints all single |
+ * digit numbers of base 10 starting	   |
+ * from 0, followed by a new line.	   |
+ * -You are not allowed to use any variable|
+ * of type char				   |
+ * -All your code should be in the main	   |
+ * function				   |
+ * =========================================
+ */
 int main(void)
 {
 	int number = 47;
 
-	while(number <= 56)
+	while (number <= 56)
 	{
 		number++;
 		putchar(number);
