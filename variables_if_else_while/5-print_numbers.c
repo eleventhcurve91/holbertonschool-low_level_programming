b33e702eefc1728be208232c0677c82734f38ce6
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-12 21:20:31 -0700

    \(*@*)/

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Starting program
+ * Return: Always 0 (Succes)
+ * =======================================
+ * Autor: Carlos Alfonso Bustos Olaya	 |
+ * =======================================
+ * ************Description****************
+ * =======================================
+ * Write a program that prints all single|
+ * digit numbers of base 10 starting	 |
+ * from 0, followed by a new line.	 |
+ * =======================================
+ */
 int main(void)
 {
 	int number;
 
-	for (number = 0; number <=9; number++)
+	for (number = 0; number <= 9; number++)
+	{
 		printf("%d", number);
+	}
 	putchar('\n');
 	return (0);
 }
