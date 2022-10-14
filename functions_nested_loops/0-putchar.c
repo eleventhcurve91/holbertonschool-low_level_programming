1c416be69e18fa40a92a50548acdfc09514340e5
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-13 20:41:21 -0700

    \(*@*)/

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
-#include <unistd.h>
+#include "onepiece.h"
 
 int main(void)
 {
-	int _putchar(char c)
-	{
+	int x;
 	char letter[9] = "_putchar";
 
-	for (int x; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 		_putchar(letter[x]);
-	return (write(1, &c, 2));
-	}
+
 	return (0);
 }
