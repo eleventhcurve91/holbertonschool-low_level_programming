ac6dd423c9b067bd533c3c74d965a4dc68b3bee4
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 15:35:30 -0700

    \(*@*)/

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,20 +6,10 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; x <= 9; y++)
+		for (y = 0; y <= 9; y++)
 		{
 			xy = x*y;
-		}
-		if (xy == 0)
-		{
-			_putchar(xy +'0');
-		}
-		if (xy < 10 && j != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
 			_putchar(xy + '0');
 		}
-	}
 	putchar(10);
 }
