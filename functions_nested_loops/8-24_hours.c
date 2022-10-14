3dc46f1a083291c91bfb50ec9acb646e4bcbded3
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 11:50:04 -0700

    \(*@*)/

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -15,11 +15,11 @@ void jack_bauer(void)
 	{
 		for(min = 0; min <= 59; min++)
 		{
-			putchar((hour / 10) + '10');
-			putchar((hour % 10) + '10');
+			putchar((hour / 10) + '0');
+			putchar((hour % 10) + '0');
 			putchar(':');
-			putchar((min /10) + '10');
-			putchar((min % 10) + '10');
+			putchar((min /10) + '0');
+			putchar((min % 10) + '0');
 			putchar(10);
 		}
 	}
