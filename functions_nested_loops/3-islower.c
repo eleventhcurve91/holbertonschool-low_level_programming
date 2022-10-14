568e88822dbfbf3c0c51e111e241e6c3eb372882
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 02:59:08 -0700

    \(*@*)/

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Write a function that checks for lowercase character.
- *
+ * @c: the character to check
  * Return: 1 if c is lowercase, Returns 0 otherwise
  */
 int _islower(int c)
