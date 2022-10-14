4f61d3a43ca2d8eea6fb1914ed58b58968b30e1f
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 03:06:04 -0700

    \(*@*)/

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Write a function that checks for lowercase character.
+ *
+ * @c: the character to check
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase, Returns 0 otherwise
+ *
+ */
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z');
+}
