94355f75e249f10799e2d62a54ce40273fa839b7
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-13 21:13:47 -0700

    \(*@*)/

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
+/**
+ * Write a program that prints _putchar, followed by a new line.
+ * The program should return 0
+ */
+
 int main(void)
 {
 	int x;
-	char letter[7] = "_putchar";
+	char letter[8] = "_putchar";
 
 	for (x = 0; x <= 7; x++)
 		_putchar(letter[x]);
