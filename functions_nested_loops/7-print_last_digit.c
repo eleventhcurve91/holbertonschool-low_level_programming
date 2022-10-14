d312f986bc10cc971d53cccb932dc15f4748becb
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 11:06:52 -0700

    \(*@*)/

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - Write a function that prints the last digit of a number.
- * @n: int to check
+ * @d: int to check
  * Return: Always 0.
  */
 int print_last_digit(int d)
