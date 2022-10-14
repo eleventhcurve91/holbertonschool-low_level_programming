c6c10a65587495bead2d9c8f2f622b68dee64ce5
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-13 21:50:28 -0700

    \(*@*)/

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ */
+
+int print_alphabet();
+{
+	int prey;
+
+	for (prey = 'a'; prey <='z'; prey++)
+		_putchar(prey);
+
+	return (0);
+}
