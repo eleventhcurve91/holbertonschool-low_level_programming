fb16336b229dae790383046cd5f2ce80f8d3a61c
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 02:22:58 -0700

    \(*@*)/

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "mian.h"
+
+/*
+ * _islower - Write a function that checks for lowercase character.
+ * Returns 1 if c is lowercase
+ * Returns 0 otherwise
+ *
+ * Return: 0 (Succes)
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
