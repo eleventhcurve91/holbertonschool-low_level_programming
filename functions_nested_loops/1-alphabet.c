f1e4d70db693dd0d1181c2f9f50486bfda678814
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 01:41:14 -0700

    \(*@*)/

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - Write a function that prints
+ * the alphabet, in lowercase, followed by a new line.
  *
  * Return: Always 0.
  *
