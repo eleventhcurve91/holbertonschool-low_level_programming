f5d464b3bd8bd15bf16637691c34a93e22c7bee9
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 21:31:34 -0700

    \(*@*)/

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check the code 
+ * @c: check int
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
