5830c267b60faa8876b329706331db5124117e14
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-15 14:14:45 -0700

    \(*@*)/

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,7 +7,11 @@ void more_numbers(void)
 
 		for (y = 0; y <= 14; y++)
 		{
-			_putchar(y + '0');
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0');
+			}
 		}
 	x++;
 
