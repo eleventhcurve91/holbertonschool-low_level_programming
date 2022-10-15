943667635003248ea071221ae73b73ab4a7b88cb
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-15 15:14:22 -0700

    \(*@*)/

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,25 +4,15 @@ void more_numbers(void)
 {
 	int x = 0;
 	int y;
+	char number[] = {0,1,2,3,4,5,6,7,8,9,1,0,1,1,1,2,1,3,1,4}
 	
 	while(x < 10)
 	{
-		for (y = 0; y <= 15; y++)
+		for (y = 0; y <= 14; y++)
 		{
-			if (y <= 9)
-			{
-				_putchar(y + '0');
-			}
-			if ( y == 15)
-			{
-				y = '\n';
-			}
-			if(y >= 10)
-			{
-				_putchar((y / 10) + '0');
-				_putchar((y % 10) + '0');
-			}
+			_putchar(numer[y]);
 		}
-		x++;
+		i++;
+		_putchar(10);
 	}
 }
