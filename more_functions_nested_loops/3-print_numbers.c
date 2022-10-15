d4761899cf603b5223cefd5d770fe96ea09e1606
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 21:53:11 -0700

    \(*@*)/

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	int x;
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	return (0);
+}
