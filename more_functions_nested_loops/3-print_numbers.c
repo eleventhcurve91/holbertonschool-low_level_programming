1e0ce7e6f5e84c0db3065d1ee25c89627e7d6d22
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 22:13:30 -0700

    \(*@*)/

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,13 @@
  *
  * Return: Always 0.
  */
-void print_number(void)
+void print_numbers(void)
 {
 	int x;
-	for (x = 0; x <= 9; x++)
+
+	for (x = 0; x <= 10; x++)
 	{
-		printf("%d", x);
+		_putchar(x + '0');
 	}
+	_putchar(10);
 }
