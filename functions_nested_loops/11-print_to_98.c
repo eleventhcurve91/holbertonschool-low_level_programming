b5b9980a5ccf2b884b8a17895034abffc43d130c
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 19:56:33 -0700

    \(*@*)/

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,10 +2,32 @@
 
 void print_to_98(int n)
 {
-	for (n = 0; n <= 98; n++)
+	int x, y;
+
+	if (n <= 98)
 	{
-		_putchar(n);
-		_putchar(',');
-		_putchar(' ');
+		if (x = n; x <= 98; x++)
+		{
+			if (x != 98)
+			{
+				printf("%d, " x);
+			}
+			else if (x == 98)
+			{
+				printf("%d\n", x);
+			}
+		}
+	}
+	else if ( n >= 98)
+	{
+		for (y = n; y >= 98; y--)
+		{
+			if (y != 98)
+			{
+				printf("%d, ", y);
+			else if (y == 98)
+				printf("%d\n", y);
+			}
+		}
 	}
 }
