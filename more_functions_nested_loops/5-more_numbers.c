bcb8b0a1b8438bbbacd1315b0d85c803c230d5f3
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-14 22:45:44 -0700

    \(*@*)/

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int x, y;
+
+	while (x < 14)
+	{
+		for (y = 0; y < = 14; y++)
+		{
+			if (y > 10)
+			{
+				_putchar((y / 10) + '0')
+				_putchar((y % 10) + '0')
+			}
+		}
+
+	}
+
+}
