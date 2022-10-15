5aba2f328436fba06b0c473b1a29ef582a419add
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-15 15:44:18 -0700

    \(*@*)/

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+
+
+/**
+ * print_line - check the code
+ * @n: int to check 
+ * Return: Always 0.
+ */
 void print_line(int n)
 {
 	int x;
@@ -7,7 +14,6 @@ void print_line(int n)
 	for (x = 0; x < n; x++)
 	{
 		_putchar('_');
-	
 	}
 	if (n <= 0 || x <= n)
 	{
