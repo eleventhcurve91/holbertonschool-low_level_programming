99213bf44d3cbb7f03f1558586386934886e7e8c
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-16 20:35:31 -0700

    \(*@*)/

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,12 +4,16 @@ void print_square(int size)
 {
 	int x, y;
 
-	for (y = 0;y < size; y++)
+	for (y = 0; y < size; y++)
 	{
-		for (x = 0;x < size; x++)
+		for (x = 0; x < size; x++)
 		{
 			_putchar('#');
 		}
+		_putchar(10);
+	}
+	if (size <= 0)
+	{
 		_putchar(10);
 	}
 }
