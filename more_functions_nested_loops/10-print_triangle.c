01b6718a09fb01763a1278429ae10eb406813661
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-16 23:06:48 -0700

    \(*@*)/

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void print_triangle(int size)
+{
+
+	int x, y;
+
+	for (x = 1; x < size; x++)
+	{
+		for (y = x; y < size; y++)
+		{
+			_putchar(' ');
+		}
+		for (y = 1; y <= x; y++)
+		{
+			_putchar('#');
+		}
+		_putchar(10);
+	}
+	if(size <= 0)
+	{
+		_putchar(10);
+	}
+}
