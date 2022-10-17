cb9303b0acdb86d495625c87adb8d89a5038b68b
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-16 21:31:04 -0700

    \(*@*)/

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 100; x++)
+	{
+		if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (x % 3 && x % 5)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+	}
+
+
+
+
+}
