4d0f5b5bbda8673caac96f3012ddedec557f736e
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-16 19:42:04 -0700

    \(*@*)/

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,12 +6,22 @@ void print_diagonal(int n)
 
 	for (x = 0; x <= n; x++)
 	{
-		_putchar(92);
+		for (y = 0; y < n; y++)
+		{
+			if (y == x)
+			{
+				_putchar('\\');
+			}	
+			if (y < x)
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar(10);
 	}
-	if ( x == n)
+
+	if (n < 0)
 	{
-		_putchar(92);
+		_putchar('\n');
 	}
-
-
 }
