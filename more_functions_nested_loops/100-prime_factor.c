fde2d0882d90d98ea57b4471a01c5d588216bd93
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-16 23:37:56 -0700

    \(*@*)/

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+
+	long int number = 612852475143;
+	long int pull;
+	long int x;
+	
+	pull = -1;
+	while (number % 2 == 0)
+	{
+		pull = 2;
+		number /= 2;
+	}
+	for (x = 3; x <= sqrt(n); x = x + 2)
+	{
+		while (number % x == 0)
+	{
+		pull = x;
+		number = number / x;
+	}
+	if (number > 2)
+		pull = number;
+		printf("%ld\n", pull);
+		return (0);
+}
