5155c78c34d9ee06d51bde7db9c55accdeded95d
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-16 19:54:30 -0700

    \(*@*)/

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void print_square(int size)
+{
+	int x;
+
+	for (x = 0;x < size;x++)
+	{
+		_putchar('#');
+
+	}
+	if (x == || x != 0)
+	{
+		_putchar(10);
+	}
+	else if(n <= 0)
+	{
+		_putchar(10);
+	}
+}
