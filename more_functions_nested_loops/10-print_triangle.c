76d73bdc75fa8f067051d608fbf9f19d8141e367
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-16 23:12:18 -0700

    \(*@*)/

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -5,9 +5,9 @@ void print_triangle(int size)
 
 	int x, y;
 
-	for (x = 1; x < size; x++)
+	for (x = 1; x <= size; x++)
 	{
-		for (y = x; y <= size; y++)
+		for (y = x; y < size; y++)
 		{
 			_putchar(' ');
 		}
