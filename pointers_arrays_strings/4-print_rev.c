e9425a74ef4bc195c87685b033dd9fa5aaa742ba
eleventhcurve91 <carlos.bustos@correounivalle.edu.co>
2022-10-18 16:29:18 -0700

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_rev - Write a function that prints a string, in reverse, 
+ * print_rev - Write a function that prints a string, in reverse,
  * followed by a new line.
  * @s: char to check
  * Return: Always 0.
  */
-
 void print_rev(char *s)
 {
 	int x;
+
 	x = strlen(s) - 1;
 
 	while (x >= 0 && *s != '\0'){
@@ -17,6 +17,5 @@ void print_rev(char *s)
 		_putchar(s[x]);
 	}
 	x--;
-	}
 	_putchar(10);
 }
